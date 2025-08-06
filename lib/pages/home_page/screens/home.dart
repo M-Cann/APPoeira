0ef6f9defd0635621491e55e89f540dec9e57520
M-Cann <mehmetcan.espada@gmail.com>
2025-08-06 14:57:29 +0300

    Revert "test"
    This reverts commit 64de78e60f327d25b12a0ef36e238d4b351a052f.

--- lib/pages/home_page/screens/home.dart
+++ lib/pages/home_page/screens/home.dart
@@ -40,7 +40,6 @@ class _HomePageState extends State<HomePage> {
       Navigator.push(context, MaterialPageRoute(builder: (context) => LogIn()),);
     } on FirebaseAuthException catch(e) {
       Logger.red.log('Çıkış Başarısız ${e.message}');
-      print('Test');
     }
   }
 
